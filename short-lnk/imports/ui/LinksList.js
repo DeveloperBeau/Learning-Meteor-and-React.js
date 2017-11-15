71fd5a7a2611be34a802edffde492dae310b0d0d
Beau Ayres <beau@domesticcat.com.au>
2017-11-16 09:25:17 +1100

    removed unnecessary log

--- short-lnk/imports/ui/LinksList.js
+++ short-lnk/imports/ui/LinksList.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import FlipMove from 'react-flip-move';
-import { Meteor } from 'meteor/meteor';
-import { Tracker } from 'meteor/tracker';
-import { Session } from 'meteor/session';
+import React from "react";
+import FlipMove from "react-flip-move";
+import { Meteor } from "meteor/meteor";
+import { Tracker } from "meteor/tracker";
+import { Session } from "meteor/session";
 
-import { Links } from '../api/links';
-import LinksListItem from './LinksListItem';
+import { Links } from "../api/links";
+import LinksListItem from "./LinksListItem";
 
-export default class  LinksList extends React.Component {
-
+export default class LinksList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -17,10 +16,9 @@ export default class  LinksList extends React.Component {
   }
 
   componentDidMount() {
-    console.log("ComponentWillMount LinkedList");
     this.linksTracker = Tracker.autorun(() => {
-      Meteor.subscribe('links');
-      const links = Links.find({ visible: Session.get('showVisible') }).fetch();
+      Meteor.subscribe("links");
+      const links = Links.find({ visible: Session.get("showVisible") }).fetch();
       this.setState({ links });
     });
   }
@@ -33,16 +31,16 @@ export default class  LinksList extends React.Component {
   renderLinksListItems() {
     if (this.state.links.length === 0) {
       return (
-        <div className='item'>
-          <p className='item__status-message'>No Links Found</p>
+        <div className="item">
+          <p className="item__status-message">No Links Found</p>
         </div>
-      )
+      );
     }
 
-    return this.state.links.map((link) => {
+    return this.state.links.map(link => {
       const shortUrl = Meteor.absoluteUrl(link._id);
-      return <LinksListItem key={link._id} shortUrl={shortUrl} {...link}/>
-      });
+      return <LinksListItem key={link._id} shortUrl={shortUrl} {...link} />;
+    });
   }
 
   render() {
